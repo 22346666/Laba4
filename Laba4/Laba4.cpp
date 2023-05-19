945046be19c030dc06bf50f89cb4bfc6838852a6
Nikita Tkachenko <mercartar@gmail.com>
2023-05-19 23:46:03 +0300

    Final

--- Laba4/Laba4.cpp
+++ Laba4/Laba4.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
 
 vector<int> scramble(const vector<int>& binary_signal);
 vector<int> descramble(const vector<int>& binary_signal);
 void print_signal(const vector<int>& binary_signal);
 void scrambling(const vector<int>& binary_to_scramble);
+vector<int> generate_signal(int n);
 
 
 int main()
 {
-    vector<int> my_signal = { 1,0,1,0,1,0,1,0 };
-    scrambling(my_signal);
+    vector<int> signal = generate_signal(50);
+    scrambling(signal);
+    signal = generate_signal(50);
+    scrambling(signal);
+    signal = generate_signal(50);
+    scrambling(signal);
 }
 
 vector<int> scramble(const vector<int>& binary_signal)
@@ -52,12 +58,25 @@ void print_signal(const vector<int>& binary_signal)
 
 void scrambling(const vector<int>& binary_to_scramble)
 {
-    cout << "Original signal: ";
+    cout << "Original signal:    ";
     print_signal(binary_to_scramble);
     cout << "Scrambled signal: ";
     vector<int> scrambled_signal = scramble(binary_to_scramble);
     print_signal(scrambled_signal);
     cout << "Descrambled signal: ";
-    vector<int> descrambled_signal = scramble(scrambled_signal);
+    vector<int> descrambled_signal = descramble(scrambled_signal);
     print_signal(descrambled_signal);
+    cout << "-------------------------------------------" << endl;
+}
+
+vector<int> generate_signal(int n)
+{
+    random_device generator;
+    mt19937 engine(generator());
+    uniform_int_distribution<> distributor(0, 1);
+    vector<int> signal;
+    for (int i = 0; i < n; i++) {
+        signal.push_back(distributor(engine));
+    }
+    return signal;
 }
